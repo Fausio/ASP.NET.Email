7508063e481bded01553ebb360817c23c92ec235
fausio luis <luisfausio@gmail.com>
2020-10-06 08:43:14 +0200

    send email using webconfig configuracion

---
+++ ASP.NET.Email/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace ASP.NET.Email.Controllers
+{
+    public class EmailController : Controller
+    {
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string UserMail)
+        {
+           
+            string subject = "titulo do email";
+            string body = "corpo do email";
+            WebMail.Send(UserMail, subject, body, null, null, null, true, null, null, null, null, null, null);
+            ViewBag.msg = "Email enviado com sucesso!";
+            return View();
+        }
+    }
+}
--- ASP.NET.Email/Controllers/HomeController.cs
+++ ASP.NET.Email/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,6 +31,45 @@ namespace ASP.NET.Email.Controllers
 
         public ActionResult SendEmail()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("luisfausio@gmail.com", "Fausio");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "Madara1122";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
             return View();
         }
     }
